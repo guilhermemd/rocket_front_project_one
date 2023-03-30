ea0e345e63eaee5dc66cd1782cf835cf1b0de26d
guilherme <guilherme.md@gmail.com>
2023-03-30 20:55:44 -0300

    part 2 - lesson: component: avatar

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,11 @@
+import styles from "./Avatar.module.css";
+
+export function Avatar(props) {
+  const { hasBorder = true, src } = props;
+  return (
+    <img
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      src={src}
+    />
+  );
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
 
 import styles from "./Sidebar.module.css";
 
@@ -12,11 +13,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://avatars.githubusercontent.com/u/74270263?v=4"
-          alt="user picture"
-        />
+        <Avatar src="https://avatars.githubusercontent.com/u/74270263?v=4" />
         <strong>Guilherme.md</strong>
         <span>Web Developer</span>
       </div>
