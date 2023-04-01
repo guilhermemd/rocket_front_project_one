07e530d03eb10965a2c5ed8423fad19a026e0231
guilherme <guilherme.md@gmail.com>
2023-04-01 15:51:02 -0300

    feat: laste commit on project 01 js

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,5 @@
 import { format, formatDistanceToNow } from "date-fns";
-import ptBR from "date-fns/locale/pt-BR";
+// import ptBR from "date-fns/locale/pt-BR";
 import { useState } from "react";
 
 import { Avatar } from "./Avatar";
